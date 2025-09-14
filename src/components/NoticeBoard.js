31da155ee04325b4af2a238fdd9ffeb55512ca00
Darren Andijaya <z5420288@student.unsw.edu.au>
2025-09-14 18:55:55 +0900

    new noticeboard not skinny logo"

--- src/components/NoticeBoard.js
+++ src/components/NoticeBoard.js
@@ -39,42 +39,42 @@ export default function NoticeBoard() {
             </div> */}
             
             <a 
-                href="https://events.humanitix.com/unsw-kpopsoc-x-fabsoc-x-hksa-x-ssa-x-mso-y2k-pubcrawl"
+                href="https://docs.google.com/forms/d/16FEff0qctF1FPzljsM-cHA4v7lcHic9VStBjw-2sQjM/viewform?edit_requested=true"
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="group relative w-full max-md:w-[80%]">
  
                 <div className="flex justify-center items-center text-center max-md:w-full max-md:p-5 max-md:text-sm bg-white w-[100%] h-[200px] max-md:h-[120px] rounded text-xl text-gray-700 font-medium border-2 border-gray-500 hover:border-[#273b92] font-semibold tracking-wider md:hover:text-[1.3rem] hover:border-2">
-                        Y2K Pubcrawl Tickets
+                        Annual General Meeting Sign Up Form
                 </div>
                 <img src={boy} alt="internal team" className="absolute top-[-30%] right-0 md:w-[100px] md:h-[100px] w-[80px] h-[80px] object-contain opacity-0 group-hover:opacity-100 transition-opacity duration-50 z-10" />
 
             </a>
             <a 
-                href="https://docs.google.com/forms/d/e/1FAIpQLSdqrnMXyG4ocCiyL-XkggerKjjmGsKDFFG8UEVIne2vqRlEbQ/viewform"
+                href="https://docs.google.com/document/d/1xFsrjQCbHtM9DNMPqutEaFJPOHz5ZLVT8FK2mS8W3YM/edit?tab=t.0"
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="group relative w-full max-md:w-[80%]">
  
                 <div className="flex justify-center items-center text-center max-md:w-full max-md:p-5 max-md:text-sm bg-white w-[100%] h-[200px] max-md:h-[120px] rounded text-xl text-gray-700 font-medium border-2 border-gray-500 hover:border-[#273b92] font-semibold tracking-wider md:hover:text-[1.3rem] hover:border-2">
-                        Hands Up Signup Form
+                        Executive Role Descriptions
                 </div>
                 <img src={boy} alt="internal team" className="absolute top-[-30%] right-0 md:w-[100px] md:h-[100px] w-[80px] h-[80px] object-contain opacity-0 group-hover:opacity-100 transition-opacity duration-50 z-10" />
 
             </a>
             
-            {/* <a 
-                href="https://forms.gle/evkjNsro8wboyRhT9"
+            <a 
+                href="https://docs.google.com/forms/d/1vYV6BkrnNhVOjZvfQNx2AG7vAdWuUM1bO2is-hAVgaM/viewform?edit_requested=true"
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="group relative w-full max-md:w-[80%]">
  
                 <div className="flex justify-center items-center text-center max-md:w-full max-md:p-5 max-md:text-sm bg-white w-[100%] h-[200px] max-md:h-[120px] rounded text-xl text-gray-700 font-medium border-2 border-gray-500 hover:border-[#273b92] font-semibold tracking-wider md:hover:text-[1.3rem] hover:border-2">
-                        KNTQ Sign Up Form   
+                        Executive nomination form
                 </div>
                 <img src={boy} alt="internal team" className="absolute top-[-30%] right-0 md:w-[100px] md:h-[100px] w-[80px] h-[80px] object-contain opacity-0 group-hover:opacity-100 transition-opacity duration-50 z-10" />
 
-            </a> */}
+            </a>
             {/* <div className="group relative w-full max-md:w-[80%]">
                 <div 
                     className="flex justify-center items-center text-center max-md:w-full max-md:p-5 max-md:text-sm bg-white w-[100%] h-[200px] max-md:h-[120px] rounded text-xl text-gray-700 font-medium border-2 border-gray-500 hover:border-[#273b92] font-semibold tracking-wider md:hover:text-[1.3rem] hover:border-2"
