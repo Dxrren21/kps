a7c001d8ed3cf1b79f8c416f47833f3a47100135
Emmy Luu <z5599109@ad.unsw.edu.au>
2026-08-09 16:00:24 +1000

    Added 26T2 Events

--- src/data/events.js
+++ src/data/events.js
@@ -15,6 +15,15 @@
 import isacChampionship from "../assets/events2026/ISAC_Championship.png"
 import weOnFire from "../assets/events2026/WeOnFire.jpeg"
 import ateezBad from "../assets/events2026/ateez_bad.png"
+import bts2 from "../assets/events2026/bts2.png"
+import iconicByMistake from "../assets/events2026/iconicbymistake.jpg"
+import itzyNoNo from "../assets/events2026/itzythatsanono.png"
+import kozyKorner from "../assets/events2026/kozykorner.png"
+import H2HRude from "../assets/events2026/rudeh2h.png"
+import girlsetTweak from "../assets/events2026/tweak.png"
+import zb1Top5 from "../assets/events2026/zb1top5.png"
+import cortisRedRed from "../assets/events2026/cortisredred.jpg"
+import skyfallCruise from "../assets/events2026/skyfallcruise.jpg"
 
 export const events = [
     {
@@ -25,17 +34,80 @@ export const events = [
         facebookLink: "https://www.facebook.com/share/18wT62Rcin/",
     },
     {
+        category: "Social Events",
+        date: "JUL 16, 2026",
+        title: "Skyfall Cruise [UNSW x USYD x MQU]",
+        image: skyfallCruise,
+        facebookLink: "https://campus.hellorubric.com/?eid=66898",
+    },
+    {
+        category: "Social Events",
+        date: "JUN 30, 2026",
+        title: "Kozy Korner",
+        image: kozyKorner,
+        facebookLink: "https://campus.hellorubric.com/?eid=68138",
+    },
+    {
+        category: "Dance Workshops",
+        date: "AUG 7, 2026",
+        title: "Term 2 Week 10: Iconic By Mistake",
+        image: iconicByMistake,
+        facebookLink: "https://campus.hellorubric.com/?eid=71455",
+    },
+    {
         category: "Dance Workshops",
         date: "JUL 31, 2026",
         title: "Term 2 Week 9: &TEAM - We on Fire",
         image: weOnFire,
         facebookLink: "https://campus.hellorubric.com/?eid=70845",
     },
-        {
+    {
         category: "Dance Workshops",
         date: "JUL 24, 2026",
         title: "Term 2 Week 8: ATEEZ - Bad",
         image: ateezBad,
         facebookLink: "https://fb.me/e/4C9siMR5q",
+    },
+    {
+        category: "Dance Workshops",
+        date: "JUL 17, 2026",
+        title: "Term 2 Week 7: HEARS2HEARTS - Rude",
+        image: H2HRude,
+        facebookLink: "https://campus.hellorubric.com/?eid=69115",
+    },
+    {
+        category: "Dance Workshops",
+        date: "JUL 3, 2026",
+        title: "Term 2 Week 5: ZB1 - Top 5",
+        image: zb1Top5,
+        facebookLink: "https://campus.hellorubric.com/?eid=68009",
+    },
+    {
+        category: "Dance Workshops",
+        date: "JUN 26, 2026",
+        title: "Term 2 Week 3: BTS - 2.0 ",
+        image: bts2,
+        facebookLink: "https://campus.hellorubric.com/?eid=67419",
+    },
+    {
+        category: "Dance Workshops",
+        date: "JUN 19, 2026",
+        title: "Term 2 Week 3: CORTIS - RedRed",
+        image: cortisRedRed,
+        facebookLink: "https://campus.hellorubric.com/?eid=66898",
+    },
+    {
+        category: "Dance Workshops",
+        date: "JUN 12, 2026",
+        title: "Term 2 Week 2: ITZY - That's a No No",
+        image: itzyNoNo,
+        facebookLink: "https://campus.hellorubric.com/?eid=66032",
+    },
+    {
+        category: "Dance Workshops",
+        date: "JUN 6, 2026",
+        title: "Term 2 Week 1: GIRLSET - Tweak",
+        image: girlsetTweak,
+        facebookLink: "https://campus.hellorubric.com/?eid=64697",
     },
 ]
